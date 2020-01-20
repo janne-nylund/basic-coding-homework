cf43c39eb5ab55f27c7104fe12e3bb68fc249cf2
janne-nylund <janne.nylund@centria.fi>
2020-01-20 19:56:25 +0200

    Exercise 33 done

--- part1/conditionals/exercise_33/Program.cs
+++ part1/conditionals/exercise_33/Program.cs
@@ -7,6 +7,20 @@ namespace exercise_33
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give the first string:");
+      string firstInput = Console.ReadLine();
+
+      Console.WriteLine("Give the second string:");
+      string secondInput = Console.ReadLine();
+
+      if (firstInput == secondInput)
+      {
+        Console.WriteLine("Echo!");
+      }
+      else
+      {
+        Console.WriteLine("Nope!");
+      }
 
     }
   }
