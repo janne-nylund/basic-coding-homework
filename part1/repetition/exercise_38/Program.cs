6328a5b7401af94c99c6c64df4512eb7cd8ec06c
janne-nylund <janne.nylund@centria.fi>
2020-01-20 23:29:44 +0200

    Exercise 38 done

--- part1/repetition/exercise_38/Program.cs
+++ part1/repetition/exercise_38/Program.cs
@@ -7,6 +7,23 @@ namespace exercise_38
     public static void Main(string[] args)
     {
       // Write your code here:
+      int countNegative = 0;
+
+      while (true) 
+      {
+        Console.WriteLine("Give a number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number == 0) 
+        {
+          break;
+        }
+        if (number < 0)
+        {
+          countNegative++;
+        }
+      }
+
+      Console.WriteLine("Total amount of negative numbers: " + countNegative);
 
     }
   }
