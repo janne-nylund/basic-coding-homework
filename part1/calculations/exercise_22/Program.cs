cd68d6f969293fde2bca2940e345e373dc55e4c9
janne-nylund <janne.nylund@centria.fi>
2020-01-20 14:48:06 +0200

    Exercise 22 done

--- part1/calculations/exercise_22/Program.cs
+++ part1/calculations/exercise_22/Program.cs
@@ -8,6 +8,23 @@ namespace exercise_22
     {
 
       // Write your code here:
+      // Write your code here:
+      Console.WriteLine("Give the first number!");
+      string firstInput = Console.ReadLine();
+
+      Console.WriteLine("Give the second number!");
+      string secondInput = Console.ReadLine();
+
+      Console.WriteLine("Give the third number!");
+      string thirdInput = Console.ReadLine();
+
+      int firstInt = Convert.ToInt32(firstInput);
+      int secondInt = Convert.ToInt32(secondInput);
+      int thirdInt = Convert.ToInt32(thirdInput);
+
+      double average = (firstInt + secondInt + thirdInt) / 3.0;
+
+      Console.WriteLine("The average is " + average);
 
     }
   }
