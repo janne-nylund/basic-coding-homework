e3ea1d98047d76f40b6a2b5bf3957b3c789ea284
janne-nylund <janne.nylund@centria.fi>
2020-01-20 19:52:53 +0200

    Exercise 32 done

--- part1/conditionals/exercise_32/Program.cs
+++ part1/conditionals/exercise_32/Program.cs
@@ -7,6 +7,17 @@ namespace exercise_32
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Speak, friend, and enter!");
+      string answer = Console.ReadLine();
+
+      if (answer == "Mellon")
+      {
+        Console.WriteLine("Welcome, friend!");
+      }
+      else
+      {
+        Console.WriteLine("They've got a cave troll!");
+      }
 
     }
   }
