d2fce1acd7ebe58472e33705336f36fbcb7b6496
janne-nylund <janne.nylund@centria.fi>
2020-01-20 21:00:02 +0200

    Exercise 36 done

--- part1/repetition/exercise_36/Program.cs
+++ part1/repetition/exercise_36/Program.cs
@@ -7,6 +7,25 @@ namespace exercise_36
     public static void Main(string[] args)
     {
       // Write your code here:
+      while (true) 
+      {
+        Console.WriteLine("Give a number:");
+        int userNum = Convert.ToInt32(Console.ReadLine());
+        if (userNum == 0) 
+        {
+          break;
+        }
+        else if (userNum > 0)
+        {
+          Console.WriteLine(userNum * userNum);
+        }
+        else 
+        {
+          Console.WriteLine("That is negative");
+
+        }
+
+      }
 
     }
   }
