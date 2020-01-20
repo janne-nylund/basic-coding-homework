ed9fae9e62c5fb248f31aacc792db98087723e30
janne-nylund <janne.nylund@centria.fi>
2020-01-20 09:55:44 +0200

    Exercise 10 done

--- part1/printing/exercise_10/Program.cs
+++ part1/printing/exercise_10/Program.cs
@@ -7,6 +7,22 @@ namespace exercise_10
         public static void Main(string[] args)
         {
             // Write your code here:
+            Console.WriteLine("I will tell a story, but I need some information.");
+            Console.WriteLine("Give a name for main character:");
+
+            string charName = Console.ReadLine();
+
+            Console.WriteLine("Give the character a profession:");
+
+            string charProf = Console.ReadLine();
+
+            Console.WriteLine("Here is the story:\n" +
+            "Once upon a time there was a " + charProf + " called " + charName + "\n" +
+            "On her way to work, " + charName + " often pondered what being " + charProf + " meant to them.\n" +
+            "When you work as a " + charProf + " you meet interesting people.\n" +
+            charName + " enjoys their work as " + charProf + ", The end.");
+
+
 
         }
     }
