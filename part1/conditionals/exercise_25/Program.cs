f989e0aa90a5d0015ee1467df7990463d98c7a5a
janne-nylund <janne.nylund@centria.fi>
2020-01-20 16:04:29 +0200

    Exercise 25 done

--- part1/conditionals/exercise_25/Program.cs
+++ part1/conditionals/exercise_25/Program.cs
@@ -7,6 +7,15 @@ namespace exercise_25
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give a number:");
+      string userInput = Console.ReadLine();
+
+      int userInt = Convert.ToInt32(userInput);
+
+      if (userInt == 1984)
+      {
+        Console.WriteLine("Orwell");
+      }
 
     }
   }
