b4b3ae2b2a352ec16d6d206b4607a757717bd562
janne-nylund <janne.nylund@centria.fi>
2020-01-20 14:11:33 +0200

    Exercise 18 done

--- part1/calculations/exercise_18/Program.cs
+++ part1/calculations/exercise_18/Program.cs
@@ -8,6 +8,20 @@ namespace exercise_18
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      string firstInput = Console.ReadLine();
+
+      Console.WriteLine("Give the second number!");
+      string secondInput = Console.ReadLine();
+
+      Console.WriteLine("Give the third number!");
+      string thirdInput = Console.ReadLine();
+
+      int firstInt = Convert.ToInt32(firstInput);
+      int secondInt = Convert.ToInt32(secondInput);
+      int thirdInt = Convert.ToInt32(thirdInput);
+
+      Console.WriteLine("The sum is " + (firstInt + secondInt + thirdInt));
 
     }
   }
