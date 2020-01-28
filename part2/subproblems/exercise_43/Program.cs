8aac3402fd8f21fe444d1b1cde5eef545808d346
janne-nylund <janne.nylund@centria.fi>
2020-01-28 09:54:29 +0200

    Exercise 43 done

--- part2/subproblems/exercise_43/Program.cs
+++ part2/subproblems/exercise_43/Program.cs
@@ -7,6 +7,16 @@ namespace exercise_43
     public static void Main(string[] args)
     {
       // Write your code here:
+      int userInt = Convert.ToInt32(Console.ReadLine());
+
+      if (userInt < 0) 
+      {
+        Console.WriteLine(userInt * -1);
+      } 
+      else 
+      {
+      Console.WriteLine(userInt);
+      }
       
     }
   }
