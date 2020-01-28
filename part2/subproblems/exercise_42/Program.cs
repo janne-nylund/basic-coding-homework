37af9fb8457304dd0fdf2da78a7b98aac2cf3bc4
janne-nylund <janne.nylund@centria.fi>
2020-01-28 09:23:58 +0200

    Exercise 42 done

--- part2/subproblems/exercise_42/Program.cs
+++ part2/subproblems/exercise_42/Program.cs
@@ -8,6 +8,13 @@ namespace exercise_42
     {
 
       // Write your code here:
+      int userInt1 = Convert.ToInt32(Console.ReadLine());
+      int userInt2 = Convert.ToInt32(Console.ReadLine());
+
+      int sum = userInt1 + userInt2;
+
+      double squareRoot = Math.Sqrt(sum);
+      Console.WriteLine(squareRoot);
     
     }
   }
