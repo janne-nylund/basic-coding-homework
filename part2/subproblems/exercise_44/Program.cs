db48ab12979db0db16e8a5611754e9c70cefd38c
janne-nylund <janne.nylund@centria.fi>
2020-01-28 10:27:12 +0200

    Exercise 44 done

--- part2/subproblems/exercise_44/Program.cs
+++ part2/subproblems/exercise_44/Program.cs
@@ -8,6 +8,21 @@ namespace exercise_44
     {
 
       // Write your code here:
+      int userInt1 = Convert.ToInt32(Console.ReadLine());
+      int userInt2 = Convert.ToInt32(Console.ReadLine());
+
+      if (userInt1 > userInt2) 
+      {
+        Console.WriteLine(userInt1 + " is greater than " + userInt2 + ".");
+      }
+      else if (userInt1 < userInt2)
+      {
+        Console.WriteLine(userInt1 + " is less than " + userInt2 + ".");
+      }   
+      else 
+      {
+        Console.WriteLine(userInt1 + " is equal to " + userInt2 + ".");
+      }
 
     }
   }
