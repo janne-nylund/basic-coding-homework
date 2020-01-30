c03e8dde405ba12834a4e5bd49e9dc0a8a631683
janne-nylund <janne.nylund@centria.fi>
2020-01-30 09:38:38 +0200

    Hack exercise 48 to work

--- part2/moreLoops/exercise_48/Program.cs
+++ part2/moreLoops/exercise_48/Program.cs
@@ -8,7 +8,40 @@ namespace exercise_48
     {
 
       // Write your code here:
- 
+      Console.WriteLine("Give numbers:");
+      int userInput = Convert.ToInt32(Console.ReadLine());
+      int sum = 0;
+      int counter = 0;
+      int countEven = 0;
+      int countOdd = 0;
+
+      while (userInput != -1) 
+      {
+        sum = sum + userInput;
+        userInput = Convert.ToInt32(Console.ReadLine());
+        counter++;
+
+        if (userInput % 2 == 0)
+        {
+          countEven++;
+        }
+        else        
+        {
+          countOdd++;
+        }
+      }
+
+      double userAvg = (double)sum / counter;
+      string dotAvg = Convert.ToString(userAvg);
+      string converted = dotAvg.Replace(",", ".").Replace(".", ".");
+      // remember to change userAvg to converted in "Average: " + userAvg
+
+      Console.WriteLine("Thx! Bye!");
+      Console.WriteLine("Sum: " + sum);
+      Console.WriteLine("Numbers: " + counter);
+      Console.WriteLine("Average: " + converted);
+      Console.WriteLine("Even: " + countEven);
+      Console.WriteLine("Odd: " + countOdd);
     }
   }
 }
