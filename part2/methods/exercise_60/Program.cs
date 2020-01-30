e99b317c5202cd7ef6a9332055df1105e037ab8c
janne-nylund <janne.nylund@centria.fi>
2020-01-30 13:44:23 +0200

    Ex 60 on it way

--- part2/methods/exercise_60/Program.cs
+++ part2/methods/exercise_60/Program.cs
@@ -11,11 +11,18 @@ namespace exercise_60
       // call the print command n times
       // in the end print a line break with the comand
       // Console.WriteLine("");
+      for (int i = 0; i < number; i++)
+      {
+        Console.Write("*");
+      }
+      Console.WriteLine("");
     }
 
     public static void Main(String[] args)
     {
-
+      PrintStars(5);
+      PrintStars(3);
+      PrintStars(9);
     }
 
   }
