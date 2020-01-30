74ef6a51bdc67359b7f1eb69c3bacff0e8a0d3a8
janne-nylund <janne.nylund@centria.fi>
2020-01-30 16:16:22 +0200

    Exercise 61 done

--- part2/methods/exercise_61/Program.cs
+++ part2/methods/exercise_61/Program.cs
@@ -6,26 +6,57 @@ namespace exercise_61
   {
     public static void Main(String[] args)
     {
-
+      //PrintSpaces(4);
+      //PrintStars(4);
+      //PrintRightTriangle(4);
+      ChristmasTree(10);
     }
 
-    public static void PrintStars(int v)
+    public static void PrintStars(int stars)
     {
-
+      for (int i = 0; i < stars; i++)
+      {
+        Console.Write("*");
+      }
+      Console.WriteLine("");
     }
 
-    public static void PrintSpaces(int v)
+    public static void PrintSpaces(int spaces)
     {
-
+      for (int i = 0; i < spaces; i++)
+      {
+        Console.Write(" ");
+      }
     }
 
-    public static void PrintRightTriangle(int v)
+    public static void PrintRightTriangle(int size)
     {
-
+      int sizeLeft = size -1;
+      for (int i = 1; i <= size; i++)
+      {
+        PrintSpaces(sizeLeft);
+        PrintStars(i);
+        sizeLeft--;
+      }
     }
 
-    public static void ChristmasTree(int v)
+    public static void ChristmasTree(int size)
     {
+      int sizeLeft = size - 1;
+      int rowStars = 1;
+      for (int i = 1; i <= size; i++)
+      {
+        PrintSpaces(sizeLeft);
+        PrintStars(rowStars);
+        sizeLeft--;
+        rowStars += 2;
+      }
+      int footSpaces = size - 2;
+      for (int x = 0; x < 2; x++)
+      {
+        PrintSpaces(footSpaces);
+        PrintStars(3);
+      }
 
     }
   }
