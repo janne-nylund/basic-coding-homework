7d84ebd66b851c37c63e958e84434bf000f9e698
janne-nylund <janne.nylund@centria.fi>
2020-01-30 11:36:55 +0200

    Exercise 53 done

--- part2/methods/exercise_53/Program.cs
+++ part2/methods/exercise_53/Program.cs
@@ -7,11 +7,16 @@ namespace exercise_53
     public static void Main(String[] args)
     {
       // Call your method here:
-
-
+      Division(9,2);
     }
 
     // Write your method here:
-
+    public static void Division(int numerator, int denominator)
+    {
+      double divAnswer = (double)numerator / denominator;
+      string dotAnswer = Convert.ToString(divAnswer);
+      string converted = dotAnswer.Replace(",", ".").Replace(".", ".");
+      Console.WriteLine(converted);
+    }
   }
 }
