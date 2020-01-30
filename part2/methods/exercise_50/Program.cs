66ac07678c3150e63faefe503df0ef69ce338de7
janne-nylund <janne.nylund@centria.fi>
2020-01-30 10:18:07 +0200

    Exercise 50 done

--- part2/methods/exercise_50/Program.cs
+++ part2/methods/exercise_50/Program.cs
@@ -6,13 +6,20 @@ namespace exercise_50
   {
     public static void Main(string[] args)
     {
-      // Call your method here:
- 
+      // Call your method here:      
+      Console.WriteLine("How many times?");
+      int times = Convert.ToInt32(Console.ReadLine());
+
+      for (int i = 0; i < times; i++) 
+      {
+        PrintPhrase();
+      }
     }
 
     // Write your method here:
-    public static void PrintPhrase() {
-
+    public static void PrintPhrase() 
+    {
+      Console.WriteLine("In a hole in the ground there lived a method");
     }
   }
 }
