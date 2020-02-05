f416a3a73c4fbe9cbb7657aec85ce3d801e32f77
janne-nylund <janne.nylund@centria.fi>
2020-02-05 18:32:34 +0200

    Exercise 82 done

--- part3/arrays/exercise_82/Program.cs
+++ part3/arrays/exercise_82/Program.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 
 namespace exercise_82
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // You can test your method here
-      int[] array = { 5, 1, 3, 4, 2 };
-      PrintArrayInStars(array);
+        public static void Main(string[] args)
+        {
+            // You can test your method here
+            int[] array = { 5, 1, 3, 4, 2 };
+            PrintArrayInStars(array);
 
+        }
+
+        public static void PrintArrayInStars(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int num = 0; num < array[i]; num++)
+                {
+                  Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
     }
-
-    public static void PrintArrayInStars(int[] array)
-    {
-
-    }
-  }
 }
 
