cba796950e5d06cab5393ad94509b2179439517b
janne-nylund <janne.nylund@centria.fi>
2020-02-05 16:32:51 +0200

    Exercise 72 done

--- part3/lists/exercise_72/Program.cs
+++ part3/lists/exercise_72/Program.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 
 namespace exercise_72
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            while (true)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == 9999)
+                {
+                    break;
+                }
+                list.Add(input);
+            }
 
+            int smallest = list[0];
+
+            for (int index = 0; index < list.Count; index++)
+            {
+                if (smallest > list[index])
+                {
+                    smallest = list[index];
+                }
+            }
+            Console.WriteLine("Smallest number: " + smallest);
+
+            for (int index = 0; index < list.Count; index++)
+            {
+                if (list[index] == smallest)
+                {
+                    Console.WriteLine("Found at index: " + index);
+                }
+            }
+
+        }
     }
-  }
 }
