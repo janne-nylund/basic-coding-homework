c2ec210bb5e6a2c29c4cb844ef4773bb8e118385
janne-nylund <janne.nylund@centria.fi>
2020-02-05 18:24:54 +0200

    Exercise 81 done

--- part3/arrays/exercise_81/Program.cs
+++ part3/arrays/exercise_81/Program.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 
 namespace exercise_81
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // You can test your method here
+        public static void Main(string[] args)
+        {
+            // You can test your method here
+            int[] array = { 5, 1, 3, 4, 2 };
+            PrintNeatly(array);
+        }
 
+        public static void PrintNeatly(int[] array)
+        {
+            int index = 0;
+            while (index < array.Length - 1)
+            {
+                int number = array[index];
+                Console.Write(number + ", ");
+                index = index + 1;
+            }
+            Console.Write(array[array.Length - 1] + "\n");
+        }
     }
-
-    public static void PrintNeatly(int[] array)
-    {
-
-    }
-  }
 }
 
