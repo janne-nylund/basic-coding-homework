9018b5483253c1dd955910b1109dfc00a4efd3ca
janne-nylund <janne.nylund@centria.fi>
2020-02-05 16:27:30 +0200

    Exercise 71 done

--- part3/lists/exercise_71/Program.cs
+++ part3/lists/exercise_71/Program.cs
@@ -3,21 +3,32 @@ using System.Collections.Generic;
 
 namespace exercise_71
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
+        public static void Main(string[] args)
         {
-          break;
-        }
-        list.Add(input);
-      }
+            List<int> list = new List<int>();
+            while (true)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == -1)
+                {
+                    break;
+                }
+                list.Add(input);
+            }
 
+            Console.WriteLine("Search for?");
+            int seekFor = Convert.ToInt32(Console.ReadLine());
+
+            for (int index = 0; index < list.Count; index++)
+            {
+                if (list[index] == seekFor)
+                {
+                    Console.WriteLine(seekFor + " is at index " + index);
+                }
+            }
+
+        }
     }
-  }
 }
