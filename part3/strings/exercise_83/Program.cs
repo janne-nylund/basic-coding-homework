3b3156bde201f7f60ee5279134eef652d61bcf08
janne-nylund <janne.nylund@centria.fi>
2020-02-05 20:11:34 +0200

    Exercise 83 done

--- part3/strings/exercise_83/Program.cs
+++ part3/strings/exercise_83/Program.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 
 namespace exercise_83
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-
+        public static void Main(string[] args)
+        {
+            Console.Write("Give a word: ");
+            string word = Console.ReadLine();
+            Console.WriteLine(word + word + word);
+        }
 
     }
-
-  }
 }
 
