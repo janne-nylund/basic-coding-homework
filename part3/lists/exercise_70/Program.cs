d5c04ddd70650960a005fb5045f2349e0c05d935
janne-nylund <janne.nylund@centria.fi>
2020-02-05 16:25:02 +0200

    Exercise 70 done

--- part3/lists/exercise_70/Program.cs
+++ part3/lists/exercise_70/Program.cs
@@ -3,20 +3,30 @@ using System.Collections.Generic;
 
 namespace exercise_70
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
+        public static void Main(string[] args)
         {
-          break;
+            List<int> list = new List<int>();
+            while (true)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == -1)
+                {
+                    break;
+                }
+                list.Add(input);
+            }
+            int greatest = 0;
+
+            for (int index = 0; index < list.Count; index++)
+            {
+                if (list[index] > greatest)
+                {
+                    greatest = list[index];
+                }
+            }
+            Console.WriteLine("The greatest number: " + greatest);
         }
-        list.Add(input);
-      }
     }
-  }
 }
