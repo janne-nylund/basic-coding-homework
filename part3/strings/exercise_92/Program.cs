d1ed3a43b215ceda483db230782442ba6ef7d5a3
janne-nylund <janne.nylund@centria.fi>
2020-02-06 19:03:58 +0200

    Exercise 92 done

--- part3/strings/exercise_92/Program.cs
+++ part3/strings/exercise_92/Program.cs
@@ -3,14 +3,44 @@ using System.Collections.Generic;
 
 namespace exercise_92
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
+            int oldest = 0;
+            int age = 0;
+            string name = "";
+            int nameLength = 0;
+            int longest = 0;
+            string longestName = "";
 
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                string[] pieces = input.Split(",");
+                age = 2020 - Convert.ToInt32(pieces[1]);
+                name = pieces[0];
+                nameLength = name.Length;
+
+                if (oldest < age)
+                {
+                    oldest = age;
+                }
+               
+                if (longest < nameLength)
+                {
+                    longest = nameLength;
+                    longestName = name;
+                }
+         
+            }
+            Console.WriteLine("Longest name: " + longestName);
+            Console.WriteLine("Highest age: " + oldest);
+            
+        }
     }
-  }
 }
-
-
-
