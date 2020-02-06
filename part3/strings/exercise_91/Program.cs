179c3a11ecf013670a690689d9a2b7849a61101f
janne-nylund <janne.nylund@centria.fi>
2020-02-06 19:02:29 +0200

    Exercise 91 done

--- part3/strings/exercise_91/Program.cs
+++ part3/strings/exercise_91/Program.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 
 namespace exercise_91
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
+            int oldest = 0;
+            string name = "";
 
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                string[] pieces = input.Split(",");
+
+                if (oldest < Convert.ToInt32(pieces[1]))
+                {
+                    oldest = Convert.ToInt32(pieces[1]);
+                    name = pieces[0];
+                }
+            }
+            Console.WriteLine("Name of the oldest: " + name);
+        }
     }
-  }
 }
-
-
-
