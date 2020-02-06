1f5c1507da04d73ba27ba425ba134d5a8594f1b4
janne-nylund <janne.nylund@centria.fi>
2020-02-06 18:57:01 +0200

    Exercise 88 done

--- part3/strings/exercise_88/Program.cs
+++ part3/strings/exercise_88/Program.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 
 namespace exercise_88
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                string[] pieces = input.Split(" ");
 
+                Console.WriteLine(pieces[0]);
+
+            }
+        }
     }
-  }
 }
-
-
-
