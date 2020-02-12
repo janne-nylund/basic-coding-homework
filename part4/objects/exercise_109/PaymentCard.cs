4ec8873bd70b83db84a90a05d956a65205686d34
janne-nylund <janne.nylund@centria.fi>
2020-02-12 18:56:37 +0200

    Exercise 109 done

---
+++ part4/objects/exercise_109/PaymentCard.cs
@@ -0,0 +1,48 @@
+namespace exercise_109
+{
+    public class PaymentCard
+    {
+        private double balance;
+
+        public PaymentCard(double openingBalance)
+        {
+            // write code here
+            this.balance = openingBalance;
+        }
+
+        public void EatLunch()
+        {
+            if (this.balance >= 10.60)
+            {
+                this.balance = this.balance - 10.60;
+            }
+        }
+
+        public void DrinkCoffee()
+        {
+            if (this.balance >= 2.0)
+            {
+                this.balance = this.balance - 2.0;
+            }
+        }
+
+        public void AddMoney(double amount)
+        {
+            // write code here
+            if (amount > 0 && this.balance + amount <= 150)
+            {
+                this.balance = this.balance + amount; 
+            }
+            else if(this.balance + amount > 150)
+            {
+                this.balance = 150;
+            }
+        }
+
+        public override string ToString()
+        {
+            // write code here
+            return "The card has a balance of " + this.balance + " euros";
+        }
+    }
+}
