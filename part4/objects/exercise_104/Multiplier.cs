87b197ea346007459ba106c91f31051227d417f3
janne-nylund <janne.nylund@centria.fi>
2020-02-12 18:48:14 +0200

    Exercise 104 done

---
+++ part4/objects/exercise_104/Multiplier.cs
@@ -0,0 +1,15 @@
+namespace exercise_104 
+{
+  public class Multiplier {
+    private int number;
+    public Multiplier(int number) 
+    {
+      this.number = number;
+    }
+
+    public int Multiply(int number)
+    {
+        return this.number = this.number * number;
+    }
+  }
+}
