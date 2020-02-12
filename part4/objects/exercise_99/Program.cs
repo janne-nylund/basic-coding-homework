aa0d6d1b800730c5c11420e1d3ab39b9d99b5424
janne-nylund <janne.nylund@centria.fi>
2020-02-12 08:20:53 +0200

    Exercise 99 done

--- part4/objects/exercise_99/Program.cs
+++ part4/objects/exercise_99/Program.cs
@@ -2,18 +2,18 @@
 
 namespace exercise_99
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
 
-      DecreasingCounter counter = new DecreasingCounter(20);
-      counter.PrintValue();
+            DecreasingCounter counter = new DecreasingCounter(20);
+            counter.PrintValue();
 
-      counter.Reset();
-      counter.PrintValue();
+            counter.Reset();
+            counter.PrintValue();
+        }
     }
-  }
 }
 
 
