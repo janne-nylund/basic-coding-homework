49204464ce97166d07637794e6f0d85626a23d2b
janne-nylund <janne.nylund@centria.fi>
2020-02-13 16:31:11 +0200

    Exercise 111 done

--- part4/objectlist/exercise_111/Program.cs
+++ part4/objectlist/exercise_111/Program.cs
@@ -3,17 +3,36 @@ using System.Collections.Generic;
 
 namespace exercise_111
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // IMPLEMENT YOUR CODE IN HERE!
-      List<PersonalInformation> list = new List<PersonalInformation>();
+        public static void Main(string[] args)
+        {
+            // IMPLEMENT YOUR CODE IN HERE!
+            List<PersonalInformation> list = new List<PersonalInformation>();
 
+            while (true)
+            {
+                Console.WriteLine("First name:");
+                String firstName = Console.ReadLine();
+                if (firstName == "")
+                {
+                    break;
+                }
+                Console.WriteLine("Last name:");
+                String lastName = Console.ReadLine();
+                Console.WriteLine("Identification number:");
+                string identificationNumber = Console.ReadLine();
+                
+                list.Add(new PersonalInformation(firstName, lastName, identificationNumber));
+            }
+
+            Console.WriteLine();
+
+            foreach (PersonalInformation item in list)
+            {
+                Console.WriteLine(item.firstName + " " + item.lastName);
+
+            }
+        }
     }
-  }
 }
-
-
-
-
