08e8d379b3d593deff682bd773b31f8786c1fcb4
janne-nylund <janne.nylund@centria.fi>
2020-02-13 16:32:25 +0200

    Exercise 112 done

--- part4/objectlist/exercise_112/Program.cs
+++ part4/objectlist/exercise_112/Program.cs
@@ -3,17 +3,37 @@ using System.Collections.Generic;
 
 namespace exercise_112
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // IMPLEMENT YOUR CODE IN HERE!
+        public static void Main(string[] args)
+        {
+            // IMPLEMENT YOUR CODE IN HERE!
+            List<TelevisionProgram> list = new List<TelevisionProgram>();
 
+            while (true)
+            {
+                Console.Write("Name: ");
+                String name = Console.ReadLine();
+                if (name == "")
+                {
+                    break;
+                }
+                Console.Write("Duration: ");
+                int duration = Convert.ToInt32(Console.ReadLine());
 
+                list.Add(new TelevisionProgram(name, duration));
+            }
+            Console.WriteLine();
+            Console.Write("Program's maximum duration? ");
+            int max = Convert.ToInt32(Console.ReadLine());
+
+            foreach (TelevisionProgram item in list)
+            {
+              if (item.duration <= max)
+              {
+                Console.WriteLine(item);
+              }
+            }
+        }
     }
-  }
 }
-
-
-
-
