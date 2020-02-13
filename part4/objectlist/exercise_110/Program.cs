4ee5a3296af261eea54b4fb79d24b184e44dccde
janne-nylund <janne.nylund@centria.fi>
2020-02-13 09:02:10 +0200

    Ex 110 started

--- part4/objectlist/exercise_110/Program.cs
+++ part4/objectlist/exercise_110/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 
 namespace exercise_110
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // IMPLEMENT YOUR CODE IN HERE!
+        public static void Main(string[] args)
+        {
+            // IMPLEMENT YOUR CODE IN HERE!
+            List<Item> names = new List<Item>();
+
+
+            while (true)
+            {
+                Console.Write("Name: ");
+                String name = Console.ReadLine();
+                if (name == "")
+                {
+                    break;
+                }
+
+
+                // Add to the list a new person
+                // whose name is the previous user input
+                names.Add(new Item(name));
+            }
+
+            foreach (Item name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
     }
-  }
 }
 
 
