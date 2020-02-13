df21819c904c0bf6959e2dbffc00e5591ac07d50
janne-nylund <janne.nylund@centria.fi>
2020-02-13 16:41:06 +0200

    Exercise 114 done

--- part4/files/exercise_114/Program.cs
+++ part4/files/exercise_114/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_114
     {
         public static void Main(string[] args)
         {
+            int counter = 0;
 
+            while (true)
+            {
+                string strings = Console.ReadLine();
+                if (strings == "end")
+                {
+                    break;
+                }
+                counter++;
+            }
+            Console.WriteLine(counter);
         }
     }
 }
