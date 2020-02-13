0cccdf72a36b0bc7b075447a9312f3e886586473
janne-nylund <janne.nylund@centria.fi>
2020-02-13 16:45:23 +0200

    Exercise 117 done

--- part4/files/exercise_117/Program.cs
+++ part4/files/exercise_117/Program.cs
@@ -3,13 +3,20 @@ using System.IO;
 
 namespace exercise_117
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Ask the user for the file name and print the content of the file
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Which file should have its contents printed?");
+            string fileName = Console.ReadLine();
 
-      
+            // Ask the user for the file name and print the content of the file
+            string[] lines = File.ReadAllLines(fileName);
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
     }
-  }
 }
