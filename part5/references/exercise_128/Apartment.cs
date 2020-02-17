f6d9695658aaf95094d175e14af6a2c528c4b5d2
janne-nylund <janne.nylund@centria.fi>
2020-02-17 15:21:34 +0200

    Exercise 128 done

--- part5/references/exercise_128/Apartment.cs
+++ part5/references/exercise_128/Apartment.cs
@@ -7,33 +7,41 @@ namespace exercise_128
 
     private int rooms;
     private int squares;
-    private int princePerSquare;
+    private int pricePerSquare;
 
     public Apartment(int rooms, int squares, int pricePerSquare)
     {
       this.rooms = rooms;
       this.squares = squares;
-      this.princePerSquare = pricePerSquare;
+      this.pricePerSquare = pricePerSquare;
     }
 
     public bool LargerThan(Apartment compared)
     {
+      if (this.squares > compared.squares)
+      {
+        return true;
+      }
       return false;
     }
 
     private int Price()
     {
-      return 0;
+      return this.squares * this.pricePerSquare;
     }
 
     public int PriceDifference(Apartment compared)
     {
       // Math.Abs returns the absolute value
-      return 0;
+      return Math.Abs((this.squares * this.pricePerSquare) - (compared.squares * compared.pricePerSquare));
     }
 
     public bool MoreExpensiveThan(Apartment compared)
     {
+      if (this.Price() > compared.Price())
+      {
+        return true;
+      }
       return false;
     }
   }
