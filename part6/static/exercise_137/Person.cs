d98ca7c92c148f40b1cc00f39b185a4f7362850f
janne-nylund <janne.nylund@centria.fi>
2020-03-04 15:12:30 +0200

    Exercise 137 done

--- part6/static/exercise_137/Person.cs
+++ part6/static/exercise_137/Person.cs
@@ -1,35 +1,43 @@
 namespace exercise_136
 {
-  public class Person
-  {
-    private string name;
-    private int age;
-
-    public Person(string givenName)
+    public class Person
     {
-      this.name = givenName;
-      this.age = 0;
-    }
+        private string name;
+        private int age;
 
-    public bool IsOfAge()
-    {
-      return (this.age >= 18);
-    }
+        public Person(string givenName)
+        {
+            this.name = givenName;
+            this.age = 0;
+        }
 
-    public int HowManyNames()
-    {
-      // Do something here
-      return 0;
-    }
+        public bool IsOfAge()
+        {
+            return (this.age >= 18);
+        }
 
-    public void GrowOlder()
-    {
-      this.age++;
-    }
+        public int HowManyNames()
+        {
+            // Do something here
+            string name = this.name;
+            string[] pieces = name.Split(" ");
+            int numberOfNames = 0;
 
-    public override string ToString()
-    {
-      return this.name;
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                numberOfNames++;
+            }
+            return numberOfNames;
+        }
+
+        public void GrowOlder()
+        {
+            this.age++;
+        }
+
+        public override string ToString()
+        {
+            return this.name;
+        }
     }
-  }
 }
