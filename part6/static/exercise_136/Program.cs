1d6f0c0f6b72baba6ff7fa64b019d26ba1354643
janne-nylund <janne.nylund@centria.fi>
2020-03-04 15:05:37 +0200

    Exercise 136 done

--- part6/static/exercise_136/Program.cs
+++ part6/static/exercise_136/Program.cs
@@ -3,19 +3,32 @@ using System.Collections.Generic;
 
 namespace exercise_136
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      Person ada = new Person("Ada Lovelace");
-      Person jack = new Person("Jack The Ripper");
-      Person mike = new Person("Mike The Incredible Magic Mouse");
+        public static void Main(string[] args)
+        {
+            Person ada = new Person("Ada Lovelace");
+            Person jack = new Person("Jack The Ripper");
+            Person mike = new Person("Mike The Incredible Magic Mouse");
 
-      HowManyNames(ada);
-      HowManyNames(jack);
-      HowManyNames(mike);
+            HowManyNames(ada);
+            HowManyNames(jack);
+            HowManyNames(mike);
+        }
+
+        // Do something here
+
+        public static void HowManyNames(Person person)
+        {
+            string name = person.ToString();
+            string[] pieces = name.Split(" ");
+            int numberOfNames = 0;
+
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                numberOfNames++;
+            }
+            Console.WriteLine(person.ToString() + " has " + numberOfNames + " names.");
+        }
     }
-
-    // Do something here
-  }
 }
