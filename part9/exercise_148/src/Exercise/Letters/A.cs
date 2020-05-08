e77bdbf0aa428cf5e19de6476f8766fc587e21be
janne-nylund <janne.nylund@centria.fi>
2020-05-08 18:38:38 +0300

    Exercise 148 done

--- part9/exercise_148/src/Exercise/Letters/A.cs
+++ part9/exercise_148/src/Exercise/Letters/A.cs
@@ -1,7 +1,11 @@
 namespace Exercise
 {
-  using System;
-  public class A
-  {
-  }
+    using System;
+    public class A
+    {
+        public void APrint()
+        {
+            Console.WriteLine("A");
+        }
+    }
 }
--- part9/exercise_148/src/Exercise/Letters/C.cs
+++ part9/exercise_148/src/Exercise/Letters/C.cs
@@ -1,7 +1,11 @@
 namespace Exercise
 {
-  using System;
-  public class C
-  {
-  }
+    using System;
+    public class C : B
+    {
+        public void CPrint()
+        {
+            Console.WriteLine("C");
+        }
+    }
 }
