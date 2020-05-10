bf3624b8fe431296723c0bff24eb6648b7fc5b73
janne-nylund <janne.nylund@centria.fi>
2020-05-10 12:30:25 +0300

    Exercise 163 done

--- part11/exercise_163/src/Exercise/Person/Person.cs
+++ part11/exercise_163/src/Exercise/Person/Person.cs
@@ -1,16 +1,20 @@
 namespace Exercise
 {
-  using System;
-  public class Person
-  {
-
-    public string name { get; }
-    public int age { get; }
-
-    public Person(string name, int age)
+    using System;
+    public class Person
     {
-      this.name = name;
-      this.age = age;
+
+        public string name { get; }
+        public int age { get; }
+
+        public Person(string name, int age)
+        {
+            if (name == null || name == "" || name.Length > 40 || age < 0 || age > 120)
+            {
+                throw new ArgumentException("Something went wrong.");
+            }
+            this.name = name;
+            this.age = age;
+        }
     }
-  }
 }
