f70b4262229fd516f7b41aa0a5511528441edb59
janne-nylund <janne.nylund@centria.fi>
2020-05-10 12:19:07 +0300

    Exercise 162 done

--- part11/exercise_162/src/Exercise/Lottery/LotteryRow.cs
+++ part11/exercise_162/src/Exercise/Lottery/LotteryRow.cs
@@ -1,34 +1,49 @@
 namespace Exercise
 {
-  using System;
-  using System.Collections.Generic;
+    using System;
+    using System.Collections.Generic;
 
-  public class LotteryRow
-  {
-    private List<int> numbers;
-
-    public LotteryRow()
+    public class LotteryRow
     {
-      this.RandomizeNumbers();
-    }
+        private List<int> numbers;
 
-    public List<int> Numbers()
-    {
-      return this.numbers;
-    }
+        public LotteryRow()
+        {
+            this.RandomizeNumbers();
+        }
 
-    public bool ContainsNumber(int number)
-    {
-      // Tests whether the number is already among the randomized numbers
-      return false;
-    }
+        public List<int> Numbers()
+        {
+            return this.numbers;
+        }
 
-    public void RandomizeNumbers()
-    {
-      // initialize the list for numbers
-      this.numbers = new List<int>();
-      // Implement the randomization of the numbers by using the method ContainsNumber() here
-    }
+        public bool ContainsNumber(int number)
+        {
+            // Tests whether the number is already among the randomized numbers
+            if (this.numbers.Contains(number))
+            {
+                return true;
+            }
+            return false;
+        }
 
-  }
+        public void RandomizeNumbers()
+        {
+            // initialize the list for numbers
+            this.numbers = new List<int>();
+            // Implement the randomization of the numbers by using the method ContainsNumber() here
+            Random lottery = new Random();
+            for (int i = 0; i < 7;)
+            {
+                int lotteryNumber = lottery.Next(1, 41);
+                if (!ContainsNumber(lotteryNumber))
+                {
+                    this.numbers.Add(lotteryNumber);
+                    i++;
+                }
+            }
+            this.numbers.Sort();
+        }
+
+    }
 }
--- part11/exercise_162/src/Exercise/Program.cs
+++ part11/exercise_162/src/Exercise/Program.cs
@@ -1,21 +1,23 @@
+using System;
+using System.Collections.Generic;
+
 namespace Exercise
 {
-  using System;
-  using System.Collections.Generic;
-  public class Program
-  {
-    public static void Main(string[] args)
+
+    public class Program
     {
-      LotteryRow row = new LotteryRow();
-      List<int> lotteryNumbers = row.Numbers();
+        public static void Main(string[] args)
+        {
+            LotteryRow row = new LotteryRow();
+            List<int> lotteryNumbers = row.Numbers();
 
-      Console.WriteLine("Lottery numbers:");
-      foreach (int number in lotteryNumbers)
-      {
-        Console.Write(number + " ");
-      }
+            Console.WriteLine("Lottery numbers:");
+            foreach (int number in lotteryNumbers)
+            {
+                Console.Write(number + " ");
+            }
 
-      Console.WriteLine("");
+            Console.WriteLine("");
+        }
     }
-  }
 }
