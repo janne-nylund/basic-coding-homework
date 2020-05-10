94da8736f5114819ac0b1ef1fe91a135931143ce
janne-nylund <janne.nylund@centria.fi>
2020-05-10 10:39:40 +0300

    Exercise 159 done

--- part10/exercise_159/src/Exercise/Regex/Checker.cs
+++ part10/exercise_159/src/Exercise/Regex/Checker.cs
@@ -1,21 +1,38 @@
+using System.Text.RegularExpressions;
+
 namespace Exercise
 {
-  using System.Text.RegularExpressions;
-  public class Checker
-  {
-    public bool DayOfWeek(string str)
+    using System.Text.RegularExpressions;
+    public class Checker
     {
-      return false;
-    }
+        public bool DayOfWeek(string str)
+        {
+            Regex rgx = new Regex("^(mon|tue|wed|thu|fri|sat|sun)$");
+            if (rgx.IsMatch(str))
+            {
+                return true;
+            }
+            return false;
+        }
 
-    public bool AllVowels(string str)
-    {
-      return false;
-    }
+        public bool AllVowels(string str)
+        {
+            Regex rgx = new Regex("^(a|e|i|o|u)*$");
+            if (rgx.IsMatch(str))
+            {
+                return true;
+            }
+            return false;
+        }
 
-    public bool TimeOfDay(string str)
-    {
-      return false;
+        public bool TimeOfDay(string str)
+        {
+            Regex rgx = new Regex("^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$");
+            if (rgx.IsMatch(str))
+            {
+                return true;
+            }
+            return false;
+        }
     }
-  }
 }
